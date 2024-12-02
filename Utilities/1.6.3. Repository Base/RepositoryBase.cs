0be98f325b8e265e14d389d0f648ee4cf56b6350
joseMendoza0704 <167268205+joseMendoza0704@users.noreply.github.com>
2024-12-02 16:47:55 -0600

    RepositoryBase

--- Utilities/1.6.3. Repository Base/RepositoryBase.cs
+++ Utilities/1.6.3. Repository Base/RepositoryBase.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities.Connection_Manager;
+using Utilities.Data_Base;
 
 namespace Utilities._1._6._3._Repository_Base
 {
-    internal class RepositoryBase
+    public abstract class RepositoryBase
     {
+        public OleDbConnection _connection;
+        protected RepositoryBase()
+        {
+            DBConnection connection = new DBConnection("(local)", "Northwind", "MSOLEDBSQL.1");
+            _connection = connection.Open();
+
+        }
+
+        public abstract Data ProcessRequest(Data data);
     }
 }
