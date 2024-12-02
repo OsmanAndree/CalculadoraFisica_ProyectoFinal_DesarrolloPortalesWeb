21202c0c1608e10dd9aba34acc5243ed593cc4d1
joseMendoza0704 <167268205+joseMendoza0704@users.noreply.github.com>
2024-12-02 17:11:40 -0600

    Services

--- Application/1.2.1. Services/Services.cs
+++ Application/1.2.1. Services/Services.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Infraestructure.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities._1._6._4._Service_Base;
+using Utilities.Data_Base;
 
 namespace Application._1._2._1._Services
 {
-    internal class Services
+    public class Services
     {
+
+        public Data RequestProcess(Data data)
+        {
+            ServiceBase services = new ServiceBase();
+            Data result = new Data();
+            try
+            {
+                services.AddItem("Velocidad", new Velocidad());
+
+                result = services.RequestProcess(data.NombreMetodo, data);
+                return result;
+            }
+            catch (Exception ex)
+            {
+
+                Data error = new Data();
+
+                error.DataItems.Add(new DataItem() { Name = "Error", Value = ex.Message });
+                return error;
+            }
+
+
+        }
     }
 }
