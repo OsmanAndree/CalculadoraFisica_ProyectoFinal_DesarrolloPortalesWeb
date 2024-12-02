8bd6aac4cddd1b0f52db5f54acc0e1b5c08ba844
osman barahona <osma_barahonas01@unicah.edu>
2024-12-02 16:42:36 -0600

    ServiceBase

--- Utilities/1.6.4. Service Base/ServiceBase.cs
+++ Utilities/1.6.4. Service Base/ServiceBase.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities._1._6._3._Repository_Base;
+using Utilities.Data_Base;
 
 namespace Utilities._1._6._4._Service_Base
 {
-    internal class ServiceBase
+    public class ServiceBase
     {
+        private Dictionary<string, RepositoryBase> _repositories = new Dictionary<string, RepositoryBase>();
+        public void AddItem(string key, RepositoryBase value)
+        {
+            _repositories.Add(key, value);
+        }
+
+        public Data RequestProcess(string name, Data data)
+        {
+            if (_repositories.TryGetValue(name, out RepositoryBase repository))
+            {
+                RepositoryBase processData = _repositories[name];
+                if (processData != null)
+                {
+                    return processData.ProcessRequest(data);
+                }
+            }
+
+            return new Data();
+        }
     }
 }
