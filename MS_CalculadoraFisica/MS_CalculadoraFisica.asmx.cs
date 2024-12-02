0ba8ddcc069e654d111c6365c9789a3a1a68dd10
joseMendoza0704 <167268205+joseMendoza0704@users.noreply.github.com>
2024-12-02 17:25:42 -0600

    Metodo de Velocidad

--- MS_CalculadoraFisica/MS_CalculadoraFisica.asmx.cs
+++ MS_CalculadoraFisica/MS_CalculadoraFisica.asmx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using Utilities.Data_Base;
+using Application._1._2._1._Services;
 
 namespace MS_CalculadoraFisica
 {
