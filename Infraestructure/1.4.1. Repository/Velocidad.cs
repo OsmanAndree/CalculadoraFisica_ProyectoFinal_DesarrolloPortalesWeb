b04b6b71661846888b1051cf43f49affdefd604c
osman barahona <osma_barahonas01@unicah.edu>
2024-12-02 17:01:54 -0600

    Velocidad

--- Infraestructure/1.4.1. Repository/Velocidad.cs
+++ Infraestructure/1.4.1. Repository/Velocidad.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities.Data_Base;
+using Utilities._1._6._3._Repository_Base;
 
 namespace Infraestructure.Repository
 {
-    public class Velocidad
+    public class Velocidad : RepositoryBase
     {
-        //Comentario
+        public override Data ProcessRequest(Data data)
+        {
+            Data response = new Data();
+            response = data;
+            int result = int.Parse(data.DataItems[0].Value.ToString()) * int.Parse(data.DataItems[1].Value.ToString());
+            response.DataItems.Add(new DataItem() { Name = "Result", Value = result.ToString() });
+            return response;
+        }
     }
 }
